669158e39caaeb41e2973a98dadcdb246f056211
santipg04 <santipg_04@hotmail.com>
2021-08-11 14:56:00 -0500

    complementing the course

--- src/main/java/co/com/choucair/certification/proyectobase/userinterface/ChoucairLoginPage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/ChoucairLoginPage.java
@@ -4,9 +4,9 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-//@DefaultUrl("https://operacion.choucairtesting.com/academy/login/index.php")
+@DefaultUrl("https://operacion.choucairtesting.com/academy/login/index.php")
 
-    public class ChoucairLoginPage extends PageObject {
+    public class ChoucairLoginPage {
 
         public static final Target LOGIN_BUTTON = Target.the("button that shows us the form to login")
             .located(By.xpath("div[@class='d-lg-block']//strong[contains(text(),'Ingresar')]"));
--- src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
+++ src/main/java/co/com/choucair/certification/proyectobase/userinterface/SearchCoursePage.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-public class SearchCoursePage extends PageObject {
+public class SearchCoursePage {
 
     public static final Target BUTTON_UC = Target.the("Seleccione la universidad choucair")
             .located(By.xpath("//div[@id='universidad']//strong"));
